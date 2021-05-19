70458e018e802349ff5e0ab1d9aeae66e8f8ca6d
Michelle Tolfa <michelle.tolfa@freshconsulting.com>
2021-05-19 10:34:17 -0700

    Initial folder structure

---
+++ src/git_groovers/instruments/base_string_instrument.py
@@ -0,0 +1,12 @@
+from git_groovers import Chords
+
+
+class StringInstrument:
+    def __init__(self, number_of_strings: int, acoustic: bool, name: str):
+        self.number_of_strings = number_of_strings
+        self.acoustic = acoustic
+        self.name = name
+        self.type = "string"
+
+    def play_chord(chord: Chords):
+        print("playing the chord")
